66f33c63c84fab4a6ad92ed93372e94c0c6e3ede
Akshaya0106 <94048945+Akshaya0106@users.noreply.github.com>
2025-08-18 20:53:32 +0530

    Add files via upload

---
+++ Day15/Linked list cycle.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def Cycle(head):
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = head.next
+
+print(Cycle(head))
